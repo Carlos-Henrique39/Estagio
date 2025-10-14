9a3dd4b4bca5750479bbb685ebc59a43c65350a1
Carlos Henrique Benício Costa <101262618+Carlos-Henrique39@users.noreply.github.com>
2025-10-14 19:49:52 -0300

    Update index.js
    Ajustando o Router para o Vercel

--- Postagem-Estagio/mural-postagens/src/router/index.js
+++ Postagem-Estagio/mural-postagens/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import CreateAdmin from '../pages/CreateAdmin.vue';
 import Login from '../pages/Login.vue';
 import CreatePost from '../pages/CreatePost.vue';
@@ -18,7 +18,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 });
 
@@ -38,3 +38,4 @@ router.beforeEach((to, from, next) => {
 });
 
 export default router;
+
