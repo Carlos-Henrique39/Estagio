a47d9179a503cf851dad021d1d04896b9cae0e14
Carlos-Henrique39 <ique.benicio@hotmail.com>
2025-10-30 19:52:14 -0300

    Ajuste
    index.js

--- Mural-backend/index.js
+++ Mural-backend/index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
 const authRoutes = require('./routes/auth');
 const postsRoutes = require('./routes/posts');
 
@@ -12,5 +11,7 @@ app.use(express.json());
 
 app.use('/auth', authRoutes);
 app.use('/posts', postsRoutes);
+
+const PORT = process.env.PORT || 4000;
 
 module.exports = app;
