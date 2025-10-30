f7d44bbd220c9b78e72c0a018fc6c887dac7b8d3
Carlos-Henrique39 <ique.benicio@hotmail.com>
2025-10-30 18:43:49 -0300

    pasta api
    index.js

--- Mural-backend/api/index.js
+++ Mural-backend/api/index.js
@@ -5,13 +5,20 @@ const cors = require('cors');
 
 const authRoutes = require('../routes/auth');
 const postsRoutes = require('../routes/posts');
+const { pool } = require('../db');
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 app.use('/auth', authRoutes);
 app.use('/posts', postsRoutes);
+
+// Teste de conexão
+pool.connect()
+  .then(() => console.log('Conectado ao banco de dados Supabase!'))
+  .catch((err) => console.error('Erro ao conectar ao banco de dados:', err));
 
 module.exports = app;
 module.exports.handler = serverless(app);
