f2949de72b715bc0f10d722deb2d0cc9872f3fa2
Carlos-Henrique39 <ique.benicio@hotmail.com>
2025-10-30 02:10:08 -0300

    Supabase
    index.js

--- Mural-backend/index.js
+++ Mural-backend/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
 const postsRoutes = require('./routes/posts')
+const { pool } = require('./db')
 
 const app = express();
 app.use(cors());
@@ -12,4 +13,14 @@ app.use('/auth', authRoutes);
 app.use('/posts', postsRoutes);
 
 const PORT = process.env.PORT || 4000;
+
+pool.connect()
+    .then(() => {
+        console.log('Conectado ao banco de dados Supabase!');
+    })
+    .catch((err) => {
+        console.error('Erro ao conectar ao banco de dados:', err);
+    });
+
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
